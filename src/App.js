8d9eaf4f5a0f78a6e46560ca813427941ee4bebe
vmasc123 <75334318+vmasc123@users.noreply.github.com>
2021-03-09 12:55:27 -0500

    Added github and linked-in links to footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-    </div>);
+      <h1>Weather App</h1>   
+      <footer>
+        <small>
+          <a href="https://github.com/vmasc123/weather-react-app" 
+           target="_blank">
+           Open-sourced code{" "}
+          </a>
+          by <a href="https://www.linkedin.com/in/victoriamascarenhasa674a882/" target="_blank">Victoria Mascarenhas</a>
+        </small>
+      </footer>
+      </div>);
 }
 
 
